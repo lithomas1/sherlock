4c587407358ebd0efdbf8505a214ca0fbab1812a
r2dev2 <ronak.badhe@gmail.com>
2021-01-01 13:21:13 -0800

    turned \u2018 and \u2019 to '

---
+++ scripts/replace_quotes.py
@@ -0,0 +1,15 @@
+import sys
+
+file = sys.argv[1]
+replaced = []
+replace_targets = [(r"\u2018", "'"), (r"\u2019", "'")]
+
+with open(file, 'r') as fin:
+    for line in fin:
+        for targets in replace_targets:
+            line = line.replace(*targets)
+        replaced.append(line)
+
+with open(file, 'w+') as fout:
+    print(*replaced, sep='', end='', file=fout)
+
