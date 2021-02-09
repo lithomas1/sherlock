10588547518ae0845f4c04d9f65507af62281f90
r2dev2 <ronak.badhe@gmail.com>
2021-02-08 17:35:08 -0800

    can get tweets now

---
+++ sherlock/demos/twitter.py
@@ -0,0 +1,22 @@
+from typing import List
+
+import twint
+
+def get_claims(username: str, limit: int = 10) -> List[str]:
+    conf = twint.Config(**__twint_config)
+    conf.Limit = limit
+    conf.Username = username
+    tweets = conf.Store_object_tweets_list = []
+    twint.run.Search(conf)
+    return [__sanitize_tweet_of_links(t.tweet) for t in tweets]
+
+def __sanitize_tweet_of_links(tweet: str) -> str:
+    link = tweet.rfind("https://")
+    return tweet[:link].strip()
+
+
+__twint_config = {
+    "Store_object": True,
+    "Output": None,
+    "Hide_output": True
+}
