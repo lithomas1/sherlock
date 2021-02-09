c4871282fc2e82e36ee18ae2480321a0743ad4d7
r2dev2 <ronak.badhe@gmail.com>
2021-02-08 22:26:53 -0800

    changed selector div to use img instead of bg-image

--- sherlock/demos/client/src/App.svelte
+++ sherlock/demos/client/src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import InputDemo from './InputDemo.svelte';
   import TwitterDemo from './TwitterDemo.svelte';
 
   let modeStatus = 'deciding';
@@ -6,16 +7,24 @@
   const chooseInput = () => modeStatus = 'input';
   const chooseTwitter = () => modeStatus = 'twitter';
   const home = () => modeStatus = 'deciding';
+  const keyboard = 'https://images.vexels.com/media/users/3/153083/isolated/preview/9f5fcfd398a9d9c52a586cbe1d67789c-computer-keyboard-stroke-icon-by-vexels.png';
+  const twitter = 'https://static.adweek.com/adweek.com-prod/wp-content/uploads/2017/04/perspective-twitter-head-content-2017.png';
 </script>
 
 <main>
   <h1 on:click={home}>Sherlock demo</h1>
   {#if modeStatus === 'deciding'}
     <div class="button-container">
-      <div class="input-button not-phony" on:click={chooseInput} />
+      <div class="input-button not-phony" on:click={chooseInput}>
+        <img alt="select keyboard demo" src={keyboard} />
+      </div>
       <div class="separator-button" />
-      <div class="twitter-button not-phony" on:click={chooseTwitter} />
+      <div class="twitter-button not-phony" on:click={chooseTwitter}>
+        <img alt="select twitter demo" src={twitter} />
+      </div>
     </div>
+  {:else if modeStatus === 'input'}
+    <InputDemo />
   {:else if modeStatus === 'twitter'}
     <TwitterDemo />
   {/if}
@@ -40,11 +49,17 @@
     cursor: pointer;
   }
 
+  img {
+    object-fit: cover;
+    height: 40vh;
+    width: 100%;
+  }
+
   .button-container {
     margin: auto;
     padding: auto;
     width: 80vw;
-    height: 40vh;
+    max-height: 40vh;
     display: flex;
     flex-direction: row;
   }
@@ -56,9 +71,6 @@
   }
 
   .twitter-button {
-    background-image: url(https://static.adweek.com/adweek.com-prod/wp-content/uploads/2017/04/perspective-twitter-head-content-2017.png);
-    background-color: lightblue;
-    background-size: cover;
     height: 100%;
     flex-grow: 1;
   }
@@ -69,7 +81,9 @@
   }
 
   .not-phony {
+    padding: 0px;
     transition: .2s ease-in-out;
+    max-width: 40vw;
   }
 
   .not-phony:hover {
