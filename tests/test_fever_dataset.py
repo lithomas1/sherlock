599168483582aa1167fb87d2476e0b8c24a41059
Thomas Li <thomasli1234567890@gmail.com>
2021-02-21 19:16:07 -0800

    Fixups

--- tests/test_fever_dataset.py
+++ tests/test_fever_dataset.py
@@ -5,7 +5,7 @@ def test_indexing(fever_dataset):
     # Testing Nicolaj Costau Waldau which is supports
     claim, sentences, relevances = fever_dataset[0]
     assert len(sentences) == len(relevances)
-    assert (relevances[7] == np.array([0, 1, 0])).all()
+    assert (relevances[7] == 1).all()
 
 
 def test_clean_text(fever_dataset):
